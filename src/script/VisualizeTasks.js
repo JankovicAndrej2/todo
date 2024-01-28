96f3a5450f10d2c9fd19185351c0e566e067a350
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2024-01-28 18:40:16 +0100

    edit button

--- src/script/VisualizeTasks.js
+++ src/script/VisualizeTasks.js
@@ -1,3 +1,6 @@
+import { Task } from "./task_class";
+
+
 export function VisualizeTasks(){
     let parent = document.querySelector(".lists");
     let array = [];
@@ -21,6 +24,32 @@ export function VisualizeTasks(){
         img2.classList.add("img2");
         img1.src = "./img/editIcon.png";
         img2.src = "./img/deleteIcon.png";
+
+        img1.addEventListener("click", ()=>{
+            let editForm = document.querySelector(".edit-left-form");
+            editForm.style.display = "block";
+            let closeedit = document.querySelector(".edit-close-left-form");
+            let editformbutton = document.querySelector(".edit-submit-left-form");
+            let editname = document.querySelector("#edit-name");
+            let editform = document.querySelector(".edit-left-form");
+
+            editname.value = element.name;
+            closeedit.addEventListener("click", ()=>{
+                editform.style.display = "none";
+                editname.value = "";
+            }, {once:true});
+
+            editformbutton.addEventListener("click", ()=>{
+                let items = JSON.parse(localStorage.getItem("key"));
+                items[index] = new Task(editname.value);
+                editname.value = "";
+                localStorage.setItem("key", JSON.stringify(items));
+                editForm.style.display = "none";
+                VisualizeTasks();
+            }, {once: true});
+
+        })
+
 
         img2.addEventListener("click", ()=>{
             let z = JSON.parse(localStorage.getItem("key"));
