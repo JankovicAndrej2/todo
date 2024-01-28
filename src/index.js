5e8c9fb27b4f7c18277ac67a5c3b097ecd9c968f
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2024-01-28 14:15:23 +0100

    Reset

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import "./styles/style.css";
 import "./styles/normalize.css";
-
-
-
-import { openForm } from "./script/openForm.js";
-import { VisualizeArray } from "./script/vizualizeArray.js";
-
-let LeftPlus = document.querySelector(".left-plus-img");
-
-VisualizeArray();
-LeftPlus.addEventListener("click", openForm);
--- src/script/cancelLeftForm.js
+++
@@ -1,7 +0,0 @@
-export function cancelLeftForm(){
-    let name = document.querySelector("#name");
-    let leftForm = document.querySelector(".left-form");
-
-    name.value = "";
-    leftForm.style.display = "none";
-}
